4947965faa757532195dff8d961bd080fcc4436d
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-20 14:36:24 +0800

    add more test cases

--- cblas/ctest.c
+++ cblas/ctest.c
@@ -30,10 +30,75 @@ int test_cblas_dsdot() {
     return assert_eq(result, 32.0, "cblas_ddot");
 }
 
+int test_cblas_sdot() {
+    int n = 3;
+    float x[] = {1.0f, 2.0f, 3.0f};
+    float y[] = {4.0f, 5.0f, 6.0f};
+
+    float result = cblas_sdot(n, x, 1, y, 1);
+    return assert_eq(result, 32.0f, "cblas_sdot");
+}
+
+int test_cblas_ddot() {
+    int n = 3;
+    double x[] = {1.0, 2.0, 3.0};
+    double y[] = {4.0, 5.0, 6.0};
+
+    double result = cblas_ddot(n, x, 1, y, 1);
+    return assert_eq(result, 32.0, "cblas_ddot");
+}
+
+int test_cblas_sasum() {
+    int n = 3;
+    float x[] = {1.0f, -2.0f, 3.0f};
+
+    float result = cblas_sasum(n, x, 1);
+    return assert_eq(result, 6.0f, "cblas_sasum");
+}
+
+int test_cblas_dasum() {
+    int n = 3;
+    double x[] = {1.0, -2.0, 3.0};
+
+    double result = cblas_dasum(n, x, 1);
+    return assert_eq(result, 6.0, "cblas_dasum");
+}
+
+int test_cblas_ssum() {
+    int n = 3;
+    float x[] = {1.0f, -2.0f, 3.0f};
+
+    float result = cblas_ssum(n, x, 1);
+    return assert_eq(result, 2.0f, "cblas_ssum");
+}
+
+int test_cblas_dsum() {
+    int n = 3;
+    double x[] = {1.0, -2.0, 3.0};
+
+    double result = cblas_dsum(n, x, 1);
+    return assert_eq(result, 2.0, "cblas_dsum");
+}
+
+int test_cblas_scsum() {
+    int n = 3;
+    float x[] = {1.0f, -2.0f, 3.0f};
+
+    float result = cblas_sasum(n, x, 1);
+    return assert_eq(result, 6.0f, "cblas_scsum");
+}
+
 int main() {
     int failed = 0;
     failed += test_cblas_sdsdot();
     failed += test_cblas_dsdot();
+    failed += test_cblas_sdot();
+    failed += test_cblas_ddot();
+    failed += test_cblas_sasum();
+    failed += test_cblas_dasum();
+    failed += test_cblas_ssum();
+    failed += test_cblas_dsum();
+    failed += test_cblas_scsum();
     if (failed > 0) {
         printf("%d tests failed.\n", failed);
         return 1;
